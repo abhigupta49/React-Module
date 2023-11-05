a4503101e577a144e0a208e74941f9fe0834f9c0
Abhishek Kumar <guptabhi49@gmail.com>
2023-11-05 22:05:43 +0530

    Day-1 Inception Episode Modification

--- Day-1/app.js
+++ Day-1/app.js
@@ -1,5 +1,26 @@
+{/* <div>
+    <div>
+        <h1>Hi I am H1 tag</h1>
+        <h2>Hi I am H2</h2>
+    </div>
+    <div>
+        <h1>Hi I am H1 tag</h1>
+        <h2>Hi I am H2</h2>
+    </div>
+</div> */}
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-const parent = React.createElement("div",{},[React.createElement("h1",{},"Hi I Am Abhishek"),React.createElement("h2",{},"Hi I Am Abhishek")]);
+const parent = React.createElement("div",{},[
+    React.createElement("div",{},[
+        React.createElement("h1",{},"Hi I am H1 tag"),
+        React.createElement("h2",{},"Hi I am H2")
+    ]),
+    React.createElement("div",{},[
+        React.createElement("h1",{},"Hi I am H1 tag"),
+        React.createElement("h2",{},"Hi I am H2")
+    ])
+])
 
 root.render(parent)
