a42e41e7904fe376fad4ad91e935b710a88d67df
Abhishek Kumar <guptabhi49@gmail.com>
2023-11-05 21:56:04 +0530

    Day-1 Inception Episode

---
+++ Day-1/app.js
@@ -0,0 +1,5 @@
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+const parent = React.createElement("div",{},[React.createElement("h1",{},"Hi I Am Abhishek"),React.createElement("h2",{},"Hi I Am Abhishek")]);
+
+root.render(parent)
